571e1332b4f4745e68d843a90784e43da16c1677
project_site <shobasanjay.80@gmail.com>
2026-02-06 21:39:20 +0530

    Update highrisk.js

--- routes/highrisk.js
+++ routes/highrisk.js
@@ -7,6 +7,7 @@ const router = express.Router();
           { name: "Zone B", lat: 11.7379, lng: 79.7390, radius: 700, type: "low" },
           { name: "Zone C", lat: 11.9217, lng: 79.6107, radius: 500, type: "high" },
           { name: "Zone D", lat: 11.9192, lng: 79.6097, radius: 700, type: "low" },
+          {name: "Near Chennai Institute of Technology", lat: 12.9416, lng: 80.0869, radius: 400, type: "high"}
         ];
 
 router.get('/', (req, res) => res.json(zones));
