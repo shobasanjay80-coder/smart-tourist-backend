abccd21e969c109a2590ada70ac3ab08bb7b5829
Sanjay <shobasanjay.80@gmail.com>
2026-02-08 12:51:30 +0530

    fixed gemini model

--- services/aiService.js
+++ services/aiService.js
@@ -3,7 +3,8 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 
 exports.askAI = async (city, weather) => {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+
 
   const prompt = `
 User wants to travel to ${city}.
